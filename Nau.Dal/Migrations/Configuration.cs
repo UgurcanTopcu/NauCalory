5bfc646a0d4e151dd4ca8c0ef4a4560840e0d209
Uğurcan Topçu <ugurcan_1908@hotmail.com>
2022-09-12 20:07:58 +0300

    Add project files.

---
+++ Nau.Dal/Migrations/Configuration.cs
@@ -0,0 +1,29 @@
+﻿namespace Nau.Dal.Migrations
+{
+    using Nau.Dal.SeedConfiguration;
+    using System;
+    using System.Data.Entity;
+    using System.Data.Entity.Migrations;
+    using System.Linq;
+
+    internal sealed class Configuration : DbMigrationsConfiguration<Nau.Dal.NauDbContext>
+    {
+        public Configuration()
+        {
+            AutomaticMigrationsEnabled = true;
+        }
+
+        protected override void Seed(Nau.Dal.NauDbContext context)
+        {
+            //  This method will be called after migrating to the latest version.
+
+            //  You can use the DbSet<T>.AddOrUpdate() helper extension method
+            //  to avoid creating duplicate seed data.
+            new ActivityTypeSeed(context);
+            new CategorySeed(context);
+            new MealTypeSeed(context);
+            new AdminSeed(context);
+            new FoodSeed(context);
+        }
+    }
+}
---
+++ Nau.Dal/NauDbContext.cs
@@ -0,0 +1,40 @@
+﻿using Nau.Dal.EntityConfiguration;
+using Nau.Data.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Nau.Dal
+{
+    public class NauDbContext: DbContext
+    {
+        public NauDbContext()
+        {
+            Database.Connection.ConnectionString = @"Server=tcp:caloriem.database.windows.net,1433;Initial Catalog=DbNauCalory;Persist Security Info=False;User ID=sfadmin;Password=Wolwerine1;MultipleActiveResultSets=False;Encrypt=True;TrustServerCertificate=False;Connection Timeout=30;";
+        }
+
+        public DbSet<ActivityType> ActivityTypes { get; set; }
+        public DbSet<Category> Categories { get; set; }
+        public DbSet<Food> Foods { get; set; }
+        public DbSet<Meal> Meals { get; set; }
+        public DbSet<MealDetail> MealDetails { get; set; }
+        public DbSet<MealType> MealTypes { get; set; }
+        public DbSet<User> Users { get; set; }
+
+        protected override void OnModelCreating(DbModelBuilder modelBuilder)
+        {
+
+            modelBuilder.Configurations.Add(new ActivityTypeConfiguration());
+            modelBuilder.Configurations.Add(new UserConfiguration());
+            modelBuilder.Configurations.Add(new MealTypeConfiguration());
+            modelBuilder.Configurations.Add(new CategoryConfiguration());
+            modelBuilder.Configurations.Add(new MealConfiguration());
+            modelBuilder.Configurations.Add(new FoodConfiguration());
+
+            base.OnModelCreating(modelBuilder);
+        }
+    }
+}
